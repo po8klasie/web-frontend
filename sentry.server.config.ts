b978e3f33bb38ce09058fab58ddbb40623733aee
Michał Oręziak <micorix.dev@gmail.com>
2023-05-10 22:48:45 +0200

    Add Sentry server config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,3 +1,13 @@
-// Site is purely static
+import * as Sentry from '@sentry/nextjs';
+import { publicRuntimeConfig } from './src/runtimeConfig';
 
-export default null;
+const { APP_ENVIRONMENT, APP_FRONTEND_RELEASE, PUBLIC_SENTRY_DSN } = publicRuntimeConfig;
+
+if (publicRuntimeConfig.PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: PUBLIC_SENTRY_DSN,
+    release: APP_FRONTEND_RELEASE,
+    environment: APP_ENVIRONMENT,
+    tracesSampleRate: 1.0,
+  });
+}
