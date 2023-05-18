3a7fa868650de167a9f455f29fa79d8bf2fdbe9e
Michał Oręziak <micorix.dev@gmail.com>
2023-05-18 22:36:56 +0200

    Add Sentry reporting for missing project config (#15)

--- src/config/fetchProjectConfig.ts
+++ src/config/fetchProjectConfig.ts
@@ -1,7 +1,18 @@
 import { fetchData } from '../api/queryClient';
 import { stringify } from 'query-string';
+import * as Sentry from '@sentry/nextjs';
 
-export const fetchProjectConfig = (projectID: string, properties: string[]) => {
+export const fetchProjectConfig = async (projectID: string, properties: string[]) => {
   const qs = stringify({ properties });
-  return fetchData(`/project/${projectID}/?${qs}`).then((res) => res.json());
+  try {
+    Sentry.addBreadcrumb({
+      message: `Fetching project data for projectID=${projectID}`,
+      level: 'info',
+    });
+    return await fetchData(`/project/${projectID}/?${qs}`).then((res) => res.json());
+  } catch (err) {
+    Sentry.captureException(err);
+
+    throw err;
+  }
 };
