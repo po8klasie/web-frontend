aa3dc9df022af70bdd155480e6ce0712edc99b28
Michał Oręziak <micorix.dev@gmail.com>
2025-02-01 12:48:31 +0100

    Allow custom PROJECT_CONFIGS_DIR

--- src/lib/config/readProjectConfig.ts
+++ src/lib/config/readProjectConfig.ts
@@ -1,12 +1,27 @@
 import fs from 'node:fs/promises'
 import YAML from 'yaml'
 import { projectConfigSchema } from './projectConfigSchema'
+import * as path from 'path'
+
+const DEFAULT_PROJECT_CONFIGS_DIR = './configs'
+const PROJECT_CONFIGS_DIR_ENV_VAR = 'PROJECT_CONFIGS_DIR'
+
+const POSSIBLE_EXTENSIONS = ['.config.yml', '.config.yaml']
 
 const readProjectConfig = async (projectId: string) => {
-    const filename = `./configs/${projectId}.config.yml`
-    if (!(await fs.stat(filename))) {
-        throw new Error(`Project config not found for project ID: ${projectId}`)
+    const projectConfigsDir =
+        process.env[PROJECT_CONFIGS_DIR_ENV_VAR] || DEFAULT_PROJECT_CONFIGS_DIR
+
+    const filename = POSSIBLE_EXTENSIONS.map((ext) =>
+        path.join(projectConfigsDir, `${projectId}${ext}`)
+    ).find((filename) => fs.stat(filename))
+
+    if (!filename) {
+        throw new Error(
+            `Project config file not found for project ID: ${projectId}`
+        )
     }
+
     const file = await fs.readFile(filename, 'utf8')
     return projectConfigSchema.parse(YAML.parse(file))
 }
